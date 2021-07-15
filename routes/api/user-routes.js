bce4dafabfbb7b0f8190df4d07bb7172edbb6e37
Mark S <marksherm04@gmail.com>
2021-07-15 15:14:53 -0400

    added index.js to routes and added in api routes and express package, secured password after being input by user in user-routes.js in findAll and findONe

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,9 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-	User.findAll()
+	User.findAll({
+		attributes: { exclude: ['password'] }
+	})
 		.then(dbUserData => res.json(dbUserData))
 		.catch(err => {
 			console.log(err);
@@ -14,6 +16,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
 	User.findOne({
+		attributes: { exclude: ['password'] },
 		where: {
 			id: req.params.id
 		}
@@ -75,17 +78,17 @@ router.delete('/:id', (req, res) => {
 			id: req.params.id
 		}
 	})
-	.then(dbUserData => {
-		if(!dbUserData) {
-			res.status(404).json({ message: 'No user found with this id' });
-			return;
-		}
-		res.json(dbUserData);
-	})
-	.catch(err => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+		.then(dbUserData => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
