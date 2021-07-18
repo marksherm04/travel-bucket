8d2b85fb6432a7564a000ecd9590bcc9804993c2
Mark S <marksherm04@gmail.com>
2021-07-18 13:28:54 -0400

    updated the comment-routes.js so that the comments would save, switched req.body.user_id to req.session.user_id and it fixed the issue

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,11 +11,12 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+	console.log(req.session);
 	// check if session is active (logged in)
 	if (req.session) {
 		Comment.create({
 			comment_text: req.body.comment_text,
-			user_id: req.body.user_id,
+			user_id: req.session.user_id,
 			post_id: req.body.post_id
 		})
 			.then(dbCommentData => res.json(dbCommentData))
